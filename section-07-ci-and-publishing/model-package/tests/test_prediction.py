d0a39ef41ac518e639fee4017b0e0f0588d14bea
Rishikesh Dhayarkar <rishikeshdhayarkar@Rishikeshs-MacBook-Pro.local>
2024-08-07 17:23:32 -0700

    another commit

--- section-07-ci-and-publishing/model-package/tests/test_prediction.py
+++ section-07-ci-and-publishing/model-package/tests/test_prediction.py
@@ -5,6 +5,7 @@ import numpy as np
 from regression_model.predict import make_prediction
 import pytest
 
+
 @pytest.mark.filterwarnings("ignore::DeprecationWarning", "ignore::FutureWarning")
 def test_make_prediction(sample_input_data):
     # Given
